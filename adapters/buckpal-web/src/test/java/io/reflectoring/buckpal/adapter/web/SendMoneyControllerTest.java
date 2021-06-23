de49f018a5f6de8c1221a2f957e9b90749798a96
sejoung kim <sejoung@gmail.com>
2021-06-23 17:35:59 +0900

    asd

--- adapters/buckpal-web/src/test/java/io/reflectoring/buckpal/adapter/web/SendMoneyControllerTest.java
+++ adapters/buckpal-web/src/test/java/io/reflectoring/buckpal/adapter/web/SendMoneyControllerTest.java
@@ -4,13 +4,16 @@ import io.reflectoring.buckpal.application.port.in.SendMoneyUseCase;
 import io.reflectoring.buckpal.application.port.in.SendMoneyUseCase.SendMoneyCommand;
 import io.reflectoring.buckpal.domain.Account.AccountId;
 import io.reflectoring.buckpal.domain.Money;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = SendMoneyController.class)
@@ -26,15 +29,16 @@ class SendMoneyControllerTest {
 	void testSendMoney() throws Exception {
 
 		mockMvc.perform(post("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}",
-				41L, 42L, 500)
-				.header("Content-Type", "application/json"))
-				.andExpect(status().isOk());
+			41L, 42L, 500)
+			.header("Content-Type", "application/json"))
+			.andDo(print())
+			.andExpect(status().isOk());
 
 		then(sendMoneyUseCase).should()
-				.sendMoney(eq(new SendMoneyCommand(
-						new AccountId(41L),
-						new AccountId(42L),
-						Money.of(500L))));
+			.sendMoney(eq(new SendMoneyCommand(
+				new AccountId(41L),
+				new AccountId(42L),
+				Money.of(500L))));
 	}
 
 }
